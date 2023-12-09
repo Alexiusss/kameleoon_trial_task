ecde0e6af9f67bb83543be4c6eaa19d2ff118a2c
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-12-09 16:04:54 +0300

    Init

---
+++ src/main/java/com/example/kameleoon_trial_task/KameleoonTrialTaskApplication.java
@@ -0,0 +1,12 @@
+package com.example.kameleoon_trial_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KameleoonTrialTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KameleoonTrialTaskApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/kameleoon_trial_task/KameleoonTrialTaskApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.kameleoon_trial_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KameleoonTrialTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
